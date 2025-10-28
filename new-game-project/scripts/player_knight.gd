9beea0e35dc38d5823e841755e3b3c3cbdaff7d1
mohammad0012 <moad20092008@gmail.com>
2025-10-28 11:51:21 +0300

    mm

--- new-game-project/scripts/player_knight.gd
+++ new-game-project/scripts/player_knight.gd
@@ -2,26 +2,43 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var speed : float = 75
-var can_move : bool = true:
-	set(value):
-		can_move = value
-		if value == false:
-			speed = 0
-		else:
-			speed = 75
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * speed
-	move_and_slide()
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-	if can_move:
-		if velocity == Vector2.ZERO:
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("ui_left","ui_right")
+	
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
 			animated_sprite_2d.play("idle")
 		else:
 			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+	
+	# Apply movement
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-		if velocity.x < 0:
-			animated_sprite_2d.flip_h = true
-		elif velocity.x > 0:
-			animated_sprite_2d.flip_h = false
+	move_and_slide()
