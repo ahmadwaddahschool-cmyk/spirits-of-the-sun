ebe28d0d675c07ac7ae0752a841e9485194c8208
4besanii-hue <4besanii@gmail.com>
2025-10-28 10:59:58 +0300

    o

--- new-game-project/scripts/player_knight.gd
+++ new-game-project/scripts/player_knight.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 var speed : float = 75
 var can_move : bool = true:
 	set(value):
@@ -10,5 +12,16 @@ var can_move : bool = true:
 			speed = 75
 
 func _physics_process(delta):
-	velocity = input.get_vector("ui_left","ui_right","ui_up","ui_down") * speed
+	velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * speed
 	move_and_slide()
+
+	if can_move:
+		if velocity == Vector2.ZERO:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+
+		if velocity.x < 0:
+			animated_sprite_2d.flip_h = true
+		elif velocity.x > 0:
+			animated_sprite_2d.flip_h = false
